7870b09d3008a77fdc4e0832964217673cde6dc6
Jeb <jeb.baugh+git@gmail.com>
2012-02-11 10:25:16 -0600

    flashcard project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flashcard::Application.config.secret_token = 'c3b697b1897c4604315ffb4e9479a10a853fc2cd1ae31367d9247a2a20f97d5bc63480bcf963c5b3f9a4d09eef615089444dab544ebca70533ddddb9d0cd8bc3'
